c1aff19521d5b3cc58d386cc8a635b9324fe86dd
Mustafa Al-Bayati <120540908+mustafa901@users.noreply.github.com>
2026-09-14 00:01:23 -0500

    Create GenesisApp as main entry point

---
+++ Genesis/GenesisApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenesisApp.swift
+//  Genesis
+//
+//  Created by Mustafa Al-Bayati on 8/17/26.
+//
+
+import SwiftUI
+
+@main
+struct GenesisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
